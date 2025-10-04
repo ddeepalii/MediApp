1c2f92fad0aeca1431b689c972351dc2f783ee1f
username <wecp-dummy@gmail.com>
2025-10-04 12:20:52 +0000

    DashBoard Component Done

--- client/src/app/dashbaord/dashbaord.component.ts
+++ client/src/app/dashbaord/dashbaord.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashbaord',
   templateUrl: './dashbaord.component.html',
-  styleUrls: ['./dashbaord.component.scss']
+  styleUrls: ['./dashbaord.component.scss'],
 })
-export class DashbaordComponent {
-
+export class DashbaordComponent implements OnInit {
+  roleType: any;
+  username: any;
+  constructor(private authService: AuthService) { }
+  ngOnInit(): void {
+    this.roleType = this.authService.getRole;  // to get the role of the user
+    this.username = this.authService.getUsername;
+    console.log(this.username)
+  }
 }
